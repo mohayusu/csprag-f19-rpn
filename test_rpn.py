bb5dab25a2933a415da07363baf341ae04ed6a7a
mohayusu <mohayusu@umich.edu>
2019-11-02 17:12:17 -0400

    Added another test for exponentitation. Travis should fail because it is not implemented yet.

--- test_rpn.py
+++ test_rpn.py
@@ -16,6 +16,6 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
     def test_exponent(self):
-        result = rpn.calculate("3 2 ^")
-        self.assertEqual(9, result)
+        result = rpn.calculate("5 2 ^")
+        self.assertEqual(25, result)
 
